296256e97b377b82b6b1e32edbd3ed8d23a07c8a
swapniltake1 <takeswapnil8@gmail.com>
2026-01-30 04:48:28 +0000

    updating files

--- scenarios/06_data_standardization.py
+++ scenarios/06_data_standardization.py
@@ -1,7 +1,4 @@
 from pyspark.sql.functions import when
-from common.spark_session import get_spark
-
-spark = get_spark()
 
 df = spark.read.option("header", True).option("inferSchema", True)    .csv("/Volumes/raw-data/git/allfiles/BigMart Sales.csv")
 
